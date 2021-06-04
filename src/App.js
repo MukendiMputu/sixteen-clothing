5104169deaa0a2d6211fedb573c2de542cd6539c
MukendiMputu <mukendi_mputu@outlook.com>
2021-06-05 00:33:20 +0200

    completes the header component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Preloader from "./Preloader";
 import Header from "./Header";
 
 class App extends Component {
+
   render() {
+
+    var appStyle = {
+      color: "#FFDE00"
+    }
+
     return (
-      <div className="App">
+      <div className="App" style={appStyle}>
         {/* Page preloader: just for visual effects... for now */}
         {/* <Preloader/> */}
         <Header/>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './css/index.css';
 import './css/templatemo-sixteen.css'
+import './css/fontawesome.css'
+import './vendor/bootstrap/css/bootstrap.min.css'
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
